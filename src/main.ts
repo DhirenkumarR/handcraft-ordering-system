55b6cb6fb4f2c0e36d74afb1f0fd04962e466b20
Kal-EL517 <harshildarji765@gmail.com>
2025-04-03 16:19:58 +0530

    log

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   console.log('tests');
   console.log('test 2');
+  console.log('tests 233');
+  
   
   await app.listen(process.env.PORT ?? 3000);
 }
